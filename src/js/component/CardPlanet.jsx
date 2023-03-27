b353351a4ed58571fe43bd3dfde70a8645bbd932
meryvl <meryvalerolaguna@gmail.com>
2023-03-27 23:49:28 +0000

    Card add Favorite

--- src/js/component/CardPlanet.jsx
+++ src/js/component/CardPlanet.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAppContext } from "../store/context.js";
 const CardPlanet=({key , planet})=>{
+
+    const {store , actions} = useAppContext();
+    const { addFavorite } = actions;
+   const name=planet.properties.name;
     return(
 <>
 <div class="col-sm-4">
@@ -9,12 +14,12 @@ const CardPlanet=({key , planet})=>{
         <img src={`https://starwars-visualguide.com/assets/img/planets/${planet.uid}.jpg`} className="card-img" alt="..."/>
     
     <div className="card-body"> 
-    <h3 className="card-title">{planet.properties.name}</h3>
+    <h3 className="card-title">{name}</h3>
         <p>{planet.properties.population}</p>
         <p>{planet.properties.climate}</p>
         <div>
             <Link to="/characters/1" className="btn btn-primary btn-propierties">More info</Link>
-            <Link to =""><img className="iconLike" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8moNqBsueRrMKWQJeVKTeO2oHHUyfS6jr_g&usqp=CAU"/>
+            <Link to =""><img onAuxClick={()=>addFavorite(name)} className="iconLike" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8moNqBsueRrMKWQJeVKTeO2oHHUyfS6jr_g&usqp=CAU"/>
             </Link>
         </div>   
     </div>
--- src/js/component/CardVehiculos.jsx
+++ src/js/component/CardVehiculos.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAppContext } from "../store/context.js";
 const CardVehiculos=({key , vehiculos})=>{
+  const {store , actions} = useAppContext();
+  const { addFavorite } = actions;
+ const name=vehiculos.properties.name;
+
     return(
 <div class="col-sm-4">
  <div className="card-sw cardStyle " key={key}>
   <img src={`https://starwars-visualguide.com/assets/img/vehicles/${vehiculos.uid}.jpg`} className="card-img mt-2" alt="..."/>
   <div className="card-body">
-    <h3 className="card-title">{vehiculos.properties.name}</h3>
+    <h3 className="card-title">{name}</h3>
     <p>consumables:{vehiculos.properties.consumables}</p>
     <p>vehicle class:{vehiculos.properties.vehicle_class}</p>
             <div>
             <Link to="/characters/1" className="btn btn-primary btn-propierties">More info</Link>
-            <Link to =""><img className="iconLike" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8moNqBsueRrMKWQJeVKTeO2oHHUyfS6jr_g&usqp=CAU"/>
+            <Link to =""><img onClick={()=>addFavorite(name)} className="iconLike" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8moNqBsueRrMKWQJeVKTeO2oHHUyfS6jr_g&usqp=CAU"/>
             </Link>
             </div>   
   </div>
