701b360f35391e85fef7453bdb770b5a892571ac
meryvl <meryvalerolaguna@gmail.com>
2023-03-28 11:16:21 +0000

    detail planet style

--- src/js/views/DetailsPlanet.js
+++ src/js/views/DetailsPlanet.js
@@ -4,25 +4,39 @@ const DetailsPlanet =()=>{
     const { id } = useParams();
     const api =`https://www.swapi.tech/api/planets/${id}`;
     const [detailPlanet , setDetailPlanet] = useState([])
+    const [propertiesPlanet , setPropertiesPlanet]= useState([])
+
     useEffect(()=>{
     fetch(api)
     .then(res=>res.json())
     .then(res => {setDetailPlanet(res.result)
-    console.log(res.result.description)})
+    console.log(res.result.properties)
+    setPropertiesPlanet(res.result.properties)})
     .catch(err=> console.log(err))
     
     },[api])
-    console.log(api)
+    console.log(propertiesPlanet)
     
     const params= useParams();
     console.log("PARAMS:", params)
     return (
        <>
-    <h1>Details</h1>
-    <div className="container row">    
+    
+    <div className="container m-auto row"> 
+    <h1>Details</h1>   
     <img className="col-6 m-4" src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
     <div className="col-4">
+    <h1>{propertiesPlanet.name}</h1>
     <p>{detailPlanet.description}</p>
+    <p>climate: {propertiesPlanet.climate}</p>
+    <p>created: {propertiesPlanet.created}</p>
+    <p>diameter: {propertiesPlanet.diameter}</p>
+    <p>edited: {propertiesPlanet.edited}</p>
+    <p>orbital period: {propertiesPlanet.orbital_period}</p>
+    <p>population:surface water: {propertiesPlanet.surface_water}</p>
+    <p>terrain: {propertiesPlanet.terrain}</p>
+    <p>population: {propertiesPlanet.population}</p>
+    <p>population: {propertiesPlanet.population}</p>
     </div>
     </div>
     </>
